e0748962b6c6f2efc8fd204d003034710cfa3512
Jason Secrest <jlsecrest@gmail.com>
2022-07-19 08:05:08 +0000

    debugging breakage on "RETOURTYP_BK_S"

--- pd_from_data_dictionary.py
+++ pd_from_data_dictionary.py
@@ -2,12 +2,16 @@
 
 
 #%%
+import pandas as pd
 import re
-from typing import Iterator, List, Match, Union, Tuple
+from typing import Iterator, List, Match, Tuple, Dict, Any
+import IPython
 
 # %%
 
-COL_NAMES_GROUP_INDEX = 1
+DataFrame = type(pd.DataFrame())
+
+ATTRIB_NAMES_GROUP_INDEX = 1
 DEFINITION1_GROUP_INDEX = 2
 VALUES_GROUP_INDEX = 3
 DESCRIPTION2_GROUP_INDEX = 4
@@ -15,10 +19,14 @@ DESCRIPTION2_GROUP_INDEX = 4
 
 def get_desc_iter() -> Iterator[Match[str]]:
     filename = "data/Data_Dictionary.md"
-    with open(filename, "r") as dd_file:
+    with open(filename, "r", encoding="UTF-8") as dd_file:
         doc = dd_file.read()
 
-    desc_str = r"((?:\#{3}[^\n]*\n)+)((?:(?:.*)\n)(?:(?:[^-\n]*)*\n)?)((?:-[ -]+(?:[^\n]*)\n(?: {5}.*\n)?)*)?(?:\n(Dimension translations:\n(?:- .*\n)*))?"
+    desc_str = (
+        r"((?:\#{3}[^\n]*\n)+)((?:(?:.*)\n)(?:(?:[^-\n]*)*\n)?)"
+        + r"((?:-[ -]+(?:[^\n]*)\n(?: {5}.*\n)?)*)?"
+        + r"(?:\n(Dimension translations:\n(?:- .*\n)*))?"
+    )
     desc_pat = re.compile(desc_str)
     desc_iter = desc_pat.finditer(doc)
     return desc_iter
@@ -31,56 +39,81 @@ def is_empty_group(g):
         return True
 
 
-def get_col_names(desc_match: Match) -> Union[List[str], None]:
-    g = desc_match.group(COL_NAMES_GROUP_INDEX)
+def get_attrib_names(desc_match: Match) -> List[str] | None:
+    g = desc_match.group(ATTRIB_NAMES_GROUP_INDEX)
     if is_empty_group(g):
         return None
-    col_names_str = r"(?:\d\.\d\. )?(\w+)"
-    col_names = re.findall(pattern=col_names_str, string=g)
-    return col_names
+    attrib_names_str = r"(?:\d\.\d\. )?(\w+)"
+    attrib_names = re.findall(pattern=attrib_names_str, string=g)
+    return attrib_names
 
 
-def get_definition1(desc_match: Match) -> Union[str, None]:
+def get_definition(desc_match: Match) -> List[str] | None:
     g = desc_match.group(DEFINITION1_GROUP_INDEX)
     if is_empty_group(g):
         return None
     def_lines_str = r"([^\n]*)\n"
     def_lines = re.findall(pattern=def_lines_str, string=g)
-    definition1 = " ".join(def_lines)
-    return definition1
+    definition = " ".join(def_lines)
+    return [definition]
 
 
-def get_values(desc_match: Match) -> Union[Tuple[str, str], None]:
-    g = desc_match.group(VALUES_GROUP_INDEX)
-    if is_empty_group(g):
+def get_allowed_values(desc_match: Match) -> DataFrame | None:
+    match_group: str = desc_match.group(VALUES_GROUP_INDEX)
+    if is_empty_group(match_group):
         return None
     # get the VALUE and the VALUE_DESCRIPTION
     # from example line "-  2: very likely"
     # value is 2, value_description is "very likely"
-    values_line_a_str = r"- {1,2}(-?\d+): (.*)\n"
-    value_and_vd = re.findall(values_line_a_str, g)
-    if value_and_vd is None:
-        # deals with "- missing data encoded as 0"
-        values_line_a_str = "- (.*)\n"
-        value_and_vd = re.findall(values_line_a_str, g)
-        if value_and_vd:
-            value_and_vd = ["0", "missing"]
-    if value_and_vd is None:
+    symbols_and_defs_str = r"- {1,2}(-?\d+): (.*)\n((?: {5}.*\n)*)"
+    symbols_and_defs: List[Tuple(str, str)] | Tuple(str, str, str) = re.findall(
+        symbols_and_defs_str, match_group
+    )
+    if symbols_and_defs is None:
+        # deal with "- missing data encoded as 0"
+        symbols_and_defs_str = "- (.*)\n"
+        symbols_and_defs = re.findall(symbols_and_defs_str, match_group)
+        if symbols_and_defs:
+            symbols_and_defs = [("0", "missing")]
+    if symbols_and_defs is None:
         raise ValueError(
             "No definition found in match.group({VALUES_GROUP_INDEX}): {g}"
         )
-    # sometimes the value description is multiple lines
-    vd_extras_str = r" {5}(.*)\n"
-    vd_extras = re.findall(vd_extras_str, g)
-    # string it all together and return
-    value = value_and_vd[0]
-    value_description = value_and_vd[1]
-    if vd_extras:
-        value_description += " " + " ".join(vd_extras)
-    return value, value_description
+
+    # In a Tuple T in the list,
+    #   T[0] is a symbol like "0" or "W" or "-1"
+    #   T[1] is the symbol_definition associated with the symbol
+    #   T[2] (if it exists) is any number overflow lines from the definition
+    #       - Each overflow line starts with 5 spaces and ends with \n
+
+    # process each tuple
+    # separate
+    symbols = []
+    definitions = []
+    for t in symbols_and_defs:
+        #   When T[2] exists, get its definition values and merge them with T[1]
+        definition: str = None
+        if len(t) == 3:
+            extra_lines: List[str] = re.findall(pattern=" {5}([^\n]+)", string=t[2])
+            extra_lines.insert(0, t[1])
+            definition = " ".join(extra_lines)
+        else:
+            if len(t) != 2:
+                raise ValueError(f"Expected tuple of len 2 or 3. Got len: {len(t)}")
+            definition.append(t[1])
+        if definition is None:
+            raise ValueError("var 'definition' not set")
+        symbols.append(t[0])
+        definitions.append(definition)
+        # print(symbols, definitions)
+
+    return [
+        pd.Series(definitions, index=symbols, dtype="object")
+        # [symbols, definitions],
+    ]
 
 
-def get_dim_translate(desc_match: Match) -> Union[List[str], None]:
+def get_dim_translate(desc_match: Match) -> List[str] | None:
     g = desc_match.group(DESCRIPTION2_GROUP_INDEX)
     if is_empty_group(g):
         return None
@@ -89,38 +122,67 @@ def get_dim_translate(desc_match: Match) -> Union[List[str], None]:
     return dimensional_translations
 
 
-def process_desc_match(desc_match: Match[str]):
+def get_section_df(desc_match: Match[str]) -> DataFrame | None:
     if re.match("### Table of Contents", desc_match.group(1)) is None:
         info_dict = {
-            "col_names": get_col_names(desc_match),
-            "definition1": get_definition1(desc_match),
-            "allowed_values": get_values(desc_match),
-            "definition2": get_dim_translate(desc_match),
+            "attrib_names": get_attrib_names(desc_match),
+            "definition": get_definition(desc_match),
+            "allowed_values": get_allowed_values(desc_match),
+            "dim_translation": get_dim_translate(desc_match),
         }
-        return info_dict
+        col_count = len(info_dict["attrib_names"])
+        if col_count > 1:
+            # index 0 is already set, just need to fill remaining
+            for i in range(1, col_count):
+                if i >= col_count:
+                    raise ValueError("i is too large. i : {i}, col_count : {col_count}")
+                info_dict["definition"].append(info_dict["definition"][0])
+                info_dict["allowed_values"].append(info_dict["allowed_values"][0])
+        # if the dict didn't contain any columns return none
+        out_df: pd.DataFrame() = None
+        if col_count > 0:
+            out_df = pd.DataFrame().from_dict(info_dict)
+        return out_df
 
 
 # %%
-from pprint import pprint
+def next_match(desc_iter, verbose=True):
+    match = next(desc_iter)
+    section_df = get_section_df(match)
+    if verbose == True and section_df is not None:
+        # display(section_df)
+        # display(section_df.info())
+        # display(section_df.definition)
+        # display(section_df.head())
+        # section_df.head()
+        # section_df.allowed_values
+        display(section_df.head())
+        display(section_df.loc[0, "allowed_values"])
+
+    return section_df
 
 
-def test():
+# %%
+if __name__ == "__main__":
+    from pprint import pprint
+
     desc_iter = get_desc_iter()
-    i = 0
-    for match in desc_iter:
-        g2 = match.group(2)
-        print(f"MATCH #: {i}")
-        pprint(match.groups())
-        pprint(process_desc_match(match))
-        print("\n-----------\n")
-        i += 1
-        if i >= 5:
-            break
+    next(desc_iter)  # skip the empty Table Of Contents match
+    # next_match(desc_iter)
 
 
-test()
+# %%
+for i in range(469):
+    print(i)
+    section_df = next_match(desc_iter, verbose=False)
+    print(f"previous: {section_df.attrib_names}")
 
+
+section_df = next_match(desc_iter, verbose=True)
 
 # %% [markdown]
 # Check out this MD
+# %%
+
+
 # %%
